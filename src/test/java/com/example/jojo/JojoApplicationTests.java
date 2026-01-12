7dc4a538e5de43933d96c1c07f9ddad3f9e30be6
choieunsu2009 <jogg86199@gmail.com>
2026-01-13 08:57:15 +0900

    첫번째 커밋이다 이말씀이야

---
+++ src/test/java/com/example/jojo/JojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
