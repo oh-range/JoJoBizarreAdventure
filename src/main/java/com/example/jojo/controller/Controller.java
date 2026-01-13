3009ba94e8b44f057e48a28c172099d57d4f7b0d
digerati009 <ijaeyong8954@ gmail.com>
2026-01-13 10:18:19 +0900

    이재용의 커밋!!

--- src/main/java/com/example/jojo/controller/Controller.java
+++ src/main/java/com/example/jojo/controller/Controller.java
@@ -1,5 +1,11 @@
 package com.example.jojo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 @org.springframework.stereotype.Controller
 public class Controller {
+    @GetMapping("/")
+    public String leejaeyong() {
+        return "leejaeyong";
+    }
 }
