4e61907d2b3c16c1d6d7c8de4d47b2f2b54dbbc3
choieunsu2009 <jogg86199@gmail.com>
2026-01-13 11:08:01 +0900

    docker setup success

--- src/main/java/com/example/jojo/controller/Controller.java
+++ src/main/java/com/example/jojo/controller/Controller.java
@@ -4,12 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    //    @GetMapping("/")
+//    public String leejaeyong() {
+//        return "leejaeyong";
+//    }
+
     @GetMapping("/")
     public String entry(){
         return "entrypoint";
     }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return "profile";
+    }
+
     @GetMapping("/iwantgohome")
     public String iwantgohome() {
         return "iwantgohome";
+    }
+    @GetMapping("/myjob")
+    public String myjob() {
+        return "myjob";
     }
 }
