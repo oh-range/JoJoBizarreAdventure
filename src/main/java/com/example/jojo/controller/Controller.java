b4cede6c5f148a67e2af03f30c331ebf8bf8fa2d
zons2763 <kdeld1026@gmail.com>
2026-01-13 09:27:57 +0900

    profile page added

--- src/main/java/com/example/jojo/controller/Controller.java
+++ src/main/java/com/example/jojo/controller/Controller.java
@@ -1,5 +1,12 @@
 package com.example.jojo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return "profile";
+    }
 }
